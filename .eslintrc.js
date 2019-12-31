5c4855818101b6d5bb5e6911948c5e75fd4e059c
chandankkrr <chandankkrr@gmail.com>
2019-12-31 11:26:01 +1100

    ESLint warning fixes

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,5 +8,13 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
+    "plugin:react/recommended"
   ],
-};
+  settings: {
+    "react": {
+      "createClass": "createReactClass", // Regex for Component Factory to use, default to "createReactClass"
+      "pragma": "React",  // Pragma to use, default to "React"
+      "version": "detect", // React version. "detect" automatically picks the version you have installed.
+    },
+  }
+}
