b69184b966423dc7bbfabdd9c74e9ea3aa8bc915
chandankkrr <chandankkrr@gmail.com>
2020-01-01 14:35:51 +1100

    Config changes for typescript compile

--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
-
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/Social Media in the last decade/i);
-  expect(linkElement).toBeInTheDocument();
-});
